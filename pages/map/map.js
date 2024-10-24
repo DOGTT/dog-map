8dce17125126dd3f0b655e220e6f2b14a149bd6e
tidetian <tidetian@tencent.com>
2024-10-24 17:35:12 +0800

    update

--- pages/map/map.js
+++ pages/map/map.js
@@ -1,18 +1,21 @@
 // pages/map/map.js
+
+var i = 1;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    setting: { // 使用setting配置，方便统一还原
+      rotate: 0,
+      skew: 0,
+      enableRotate: true,
+    },
+    latitude: null,
+    longitude: null,
+    address: '',
+    scale: 14,
+    circles: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  onLoad: function () {
+    this.getLocation();
   },
 
   /**
@@ -62,5 +65,54 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+  reLocation: function () {
+    console.log("reLocation")
+    this.getLocation(); // 重新获取位置
+    const mapCtx = wx.createMapContext('map', this);
+    mapCtx.moveToLocation(this.latitude, this.longitude);
+  },
+  getLocation: function () {
+    // 获取用户的位置信息
+    wx.getLocation({
+      type: 'wgs84', // 返回可以用于 wx.openLocation 的经纬度
+      success: (res) => {
+        i++
+        const latitude = res.latitude; // 纬度
+        const longitude = res.longitude; // 经度
+        console.log("reLocation", latitude, longitude)
+        this.setData({
+          setting: {
+            rotate: 0,
+            skew: 0,
+            enableRotate: true
+          },
+          latitude: latitude,
+          longitude: longitude,
+          circles: [
+            {
+              latitude: latitude,
+              longitude: longitude,
+              radius: 1000, // 圆圈半径，单位米
+              fillColor: '#00FF0033', // 半透明红色
+              color: '#E6E6FA', // 圆圈边框颜色
+              strokeWidth: 0 // 边框宽度
+            }
+          ]
+        });
+        // 可以在这里调用其他API，比如获取地址信息
+        this.getAddress(latitude, longitude);
+      },
+      fail: (err) => {
+        console.error('获取位置失败', err);
+      }
+    });
+  },
+
+  getAddress: function (latitude, longitude) {
+    // 这里可以调用其他API来获取地址信息，比如腾讯地图API等
+    // 示例代码略
   }
+
+
 })
