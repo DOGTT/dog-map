cde0b7f3200743ba125fd292f00fc2e19145f483
tide <bolitt@hotmail.com>
2025-02-18 23:53:05 +0800

    dev

--- components/post-bubble/post-bubble.js
+++ components/post-bubble/post-bubble.js
@@ -7,10 +7,19 @@ Component({
     tags: Array,
     images: Array,
     text: String,
-    reactions: Array
+    reactions: Array,
   },
 
+  data: {
+    currentIndex: 0, // 当前图片的索引
+  },
   methods: {
+     // 监听轮播图切换事件
+     onSwiperChange(event) {
+      this.setData({
+        currentIndex: event.detail.current,
+      });
+    },
     onReactionTap(e) {
       const index = e.currentTarget.dataset.index;
       this.triggerEvent('reactiontap', { index });
