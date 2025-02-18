9a77652be48851198aeab1c7b16325f0af2bcf3b
tidetian <tidetian@tencent.com>
2025-02-18 18:56:27 +0800

    update

--- components/post-bubble/post-bubble.js
+++ components/post-bubble/post-bubble.js
@@ -1,24 +1,19 @@
 // components/post-bubble/post-bubble.js
 Component({
-
-  /**
-   * 组件的属性列表
-   */
   properties: {
-
+    avatarUrl: String,
+    username: String,
+    date: String,
+    tags: Array,
+    images: Array,
+    text: String,
+    reactions: Array
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    onReactionTap(e) {
+      const index = e.currentTarget.dataset.index;
+      this.triggerEvent('reactiontap', { index });
+    }
   }
-})
+});
